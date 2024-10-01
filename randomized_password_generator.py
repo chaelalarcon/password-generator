361aaf0cdc1129e6739225d21c896743e58714f1
unknown <johnmichaelalarcon2006@gmail.com>
2024-10-01 21:57:43 +0700

    function declaration and variable initialization

--- randomized_password_generator.py
+++ randomized_password_generator.py
@@ -2,3 +2,10 @@
 import random
 import string
 
+# declare a function and initialize a variable
+def password_gen (length = 12):
+    password = ''
+    characters = string.ascii_letters + string.digits + string.punctuation
+    for i in range (length):
+        password += random.choice (characters)
+    return password
